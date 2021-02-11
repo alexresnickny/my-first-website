a3094e29e7930a5bae4b563ef082407ee80f965b
alexresnickny <adr11@nyu.edu>
2021-02-11 15:51:50 -0500

    Update Jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
-<script
-  src="https://code.jquery.com/jquery-3.5.1.js"
-  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-  crossorigin="anonymous"></script>
-
 $('.photo-list-item').on('click', function() {
 	// pull out the photo url from the element's attributes
 	var cityPhotoUrl = $(this).attr('cityphoto')
